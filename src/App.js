38ecc7a152b05ddadeb1085b51a37e68af1beac4
landinij0040 <ijlandin004@gmail.com>
2022-01-13 17:54:25 -0700

    How I want the layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <>
-      <h1>My New Resume</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Navbar</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>JumboTron</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Objective</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Education</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Experience</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Skills</h1>
+          </Col>
+        </Row>  
+      </Container>
     </>
 
   );
