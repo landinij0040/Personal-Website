5167c8727cb55210705269c8be08a279cef0383f
landinij0040 <ijlandin004@gmail.com>
2022-01-18 14:45:32 -0700

    added regular font awesome

--- src/MyFooter.js
+++ src/MyFooter.js
@@ -1,13 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
-
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const MyFooter = () => (
     <>
         <footer className="bg-dark text-center text-white">
             <Container className="p-4">
                 <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/isaiah-landin-401a431a3/">
-                    <FontAwesomeIcon icon={faGoogle} />
+                    <FontAwesomeIcon icon={faEnvelope} />
                 </a>
                 <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/isaiah-landin-401a431a3/">
                     <FontAwesomeIcon icon={faLinkedinIn} />
