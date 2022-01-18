bf86018475eb2ce6a73b4a4bcde06d254862cb4a
landinij0040 <ijlandin004@gmail.com>
2022-01-18 03:07:40 -0700

    Added Button to Project

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
 const MyNavbar = () => {
     return (
@@ -9,7 +10,12 @@ const MyNavbar = () => {
             <Navbar className="bg-dark" fixed="top" bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand >Isaiah Landin</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Button variant="outline-light">Contact</Button>
+                {/* <Nav.Item>
+                    <Nav.Link>
+                        Test
+                    </Nav.Link>
+                </Nav.Item> */}
             </Container>
             </Navbar>
         </>
