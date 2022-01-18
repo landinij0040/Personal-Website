78745cfb3a4c22ab4137918efe87b56be0fa76c6
landinij0040 <ijlandin004@gmail.com>
2022-01-18 15:56:25 -0700

    Added picture to 1st slide

--- src/SkillsCarousel.js
+++ src/SkillsCarousel.js
@@ -1,21 +1,36 @@
 import Carousel from 'react-bootstrap/Carousel';
 import img_placeholder from './imgs/black.png';
+import isaiah from './imgs/Isaiah_Landin.jpg';
 
 const skills = [1,2,3,4,5,5,7,8,9,10];
 const styleCarousel = {
     marginBottom:  '3em'
+};
+const stylePicAspectRatio = {
+    width:'300px',
+    height: 'auto',
 };
 
 const SkillsCarousel = () => (
     <>
         <div style={styleCarousel}>
             <Carousel>
+                <Carousel.Item>
+                    <div style={{backgroundColor:'$cyan-100'}}>
+                        <img
+                            style={stylePicAspectRatio}
+                            src={isaiah}
+                        />
+                    </div>
+
+                </Carousel.Item>
                 {skills.map(() => (
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
                             src={img_placeholder}
                         />
+                        
                         <Carousel.Caption>
                             <h1>Main Title</h1>
                             <h2>Description</h2>
