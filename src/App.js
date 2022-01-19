356b740763b85d44895047ad95c67ffb5e14db23
landinij0040 <ijlandin004@gmail.com>
2022-01-18 18:28:05 -0700

    Added scss to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
