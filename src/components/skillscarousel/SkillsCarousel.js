0e16d6e499c76054b0458f9df4f8044c69ef87bf
landinij0040 <ijlandin004@gmail.com>
2022-02-02 20:26:38 -0700

    Home page picture

--- src/components/skillscarousel/SkillsCarousel.js
+++ src/components/skillscarousel/SkillsCarousel.js
@@ -1,6 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
 import img_placeholder from '../../imgs/black.png';
 import isaiah from '../../imgs/Isaiah_Landin.jpg';
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const skills = [1,2,3,4,5,5,7,8,9,10];
@@ -17,13 +19,27 @@ const SkillsCarousel = () => (
         <div style={styleCarousel}>
             <Carousel>
                 <Carousel.Item>
-                    <div className={'caro-back'}>
-                        <img
-                            style={stylePicAspectRatio}
-                            src={isaiah}
-                        />
-                    </div>
+                    <div className={'caro-back text-center py-5'}>
+                        <div className={'photo-container mx-auto'}>
+                            <img
+                                className={'isaiah'}
+                                style={stylePicAspectRatio}
+                                src={isaiah}
+                            />
+                            <div className={'overlay'}>
+                                <a href="https://www.linkedin.com/in/isaiah-landin-401a431a3/">
+                                    <FontAwesomeIcon icon={faLinkedinIn} className={'linked-in-img'}/>
+                                </a>
+                            </div>
+                        </div>
 
+                        <h1>
+                            My name is Isaiah Landin
+                        </h1>
+                        <h2>
+                            A professional Hello World Programmer in  <b>ANY</b> language
+                        </h2>
+                    </div>
                 </Carousel.Item>
                 {skills.map(() => (
                     <Carousel.Item>
