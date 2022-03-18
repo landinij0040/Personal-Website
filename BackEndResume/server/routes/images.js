62f3725cd89af395764a0874655b45e03333262b
landinij0040 <ijlandin004@gmail.com>
2022-03-18 10:16:36 -0400

    The api sends folder info

--- BackEndResume/server/routes/images.js
+++ BackEndResume/server/routes/images.js
@@ -1,7 +1,9 @@
 import path from 'path';
 import express from 'express';
+import fs from 'fs';
 var router = express.Router();
 var images = ['js'];
+
 router.get('/image/skills/:number', (req, res) =>{
     var number = parseInt(req.params.number);
     console.log(typeof number );
@@ -12,6 +14,19 @@ router.get('/image/skills/:number', (req, res) =>{
     console.log(thePic)
     res.header("Access-Control-Allow-Origin", "*");
     res.sendFile(thePic);    
+});
+
+router.get('/image/skills/', (req, res) => {
+    var thePath = path.join(__dirname, '../../public/images/skills');
+    var picNames = [];
+    // console.log(thePath); // TODO: Delete
+    fs.readdir(thePath, (err, files) => {
+        files.forEach( file => {
+            picNames.push(file);
+        });
+        console.log(picNames); // TODO: Delete Later
+        res.send(picNames);
+    })
 });
 
 export default router;
