ce8e7d6d8d81f55c553641c2a7aa7de08c27c41b
landinij0040 <ijlandin004@gmail.com>
2022-04-11 21:38:38 -0500

    Youtube Videos are up now

--- FrontEndResume/src/App.js
+++ FrontEndResume/src/App.js
@@ -8,6 +8,7 @@ import MyFooter from './components/myfooter/MyFooter';
 import MyOffcanvas from './components/myoffcanvas/MyOffcanvas';
 import { Container } from 'react-bootstrap';
 import Cards from './components/section-cards/SectionCards';
+import YouTube from 'react-youtube';
 
 function App() {
   // const homePage = {
@@ -37,6 +38,21 @@ function App() {
       </>,
       "education"
     ]
+  ];
+
+
+  const vids = [
+    "fC-lGtz063o",
+    "636LptRbPzI",
+    "SGFxEOEyZtQ",
+    "7w-BHRAkdJQ",
+    "P65Fhxgo1Ls",
+    "GyIB_a8nX9s",
+    "oExmaq5yfCQ",
+    "w0v-YLYcow0",
+    "OKFpBQ9tSqA",
+    "ERRp3ww32yo",
+    "ymHMVFkp_Y4"
   ]
   return (
     <>
@@ -45,7 +61,20 @@ function App() {
       <SkillsCarousel/>
       <Container className="p-5">
         <h1 style={{'fontSize':'2.5rem'}}>Making Of Website</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt augue interdum velit. </p>
+        <p>Below are some videos detailing the making of this very website. </p>
+
+
+        {vids.map((elem) => {
+          return (
+            <div className='py-2'>
+              <YouTube videoId={elem} containerStyle={{width: '100%'}}/>
+            </div>
+            
+          );
+        })}        
+          
+        
+        
       </Container>
       {altSections.map((elem, index) => {
         if(index % 2 !== 0){
